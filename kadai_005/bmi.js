59eaa9a61489f877a137eedd3bc87693213f5ee7
Shoichiro27 <johnsnow27@icloud.com>
2025-04-26 21:33:59 +0900

    Upload kadai005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+//変数宣言・値の代入(体重)
+let bodyWeight;
+bodyWeight = 68;
+
+//変数宣言・値の代入(身長)
+let height;
+height = 1.7;
+
+//変数宣言・値の代入(BMI)
+let BMI;
+BMI = bodyWeight / (height * height);
+
+//コンソールへの出力
+console.log(BMI);
